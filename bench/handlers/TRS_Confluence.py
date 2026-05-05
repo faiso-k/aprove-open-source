de8698093e086034ff96a53114f9915c6d6bc7e1
Kassing <kassing@cs.rwth-aachen.de>
2026-05-05 14:00:57 +0200

    Fix Confluence Handler for Benchmarking

--- bench/handlers/TRS_Confluence.py
+++ bench/handlers/TRS_Confluence.py
@@ -6,7 +6,7 @@ import lib.solver as s
 def run(timeout: int, benchmark: Path, cert: bool) -> str:
     with tempfile.TemporaryDirectory() as tmpdir:
         tmp = Path(tmpdir) / "input.ari"
-        s.mkinput(tmp, "(goal CONFLUENCE)", benchmark=benchmark)
+        s.mkinput(tmp, "(GOAL CONFLUENCE)", benchmark=benchmark)
         s._print_input(tmp, "TRS_Confluence")
         if cert:
             return s.run_cpf_convert(tmp)
